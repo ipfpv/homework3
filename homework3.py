344bf77f18ad91cb900f7dad2413c86a2058ed27
ipfpv <167916397+ipfpv@users.noreply.github.com>
2024-05-03 00:24:58 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,7 @@
+my_string = input("Введите дату и время: ") #00:21, 22 may 2024
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print(my_string[0])
+print(my_string[-1])
